af9b25f6190043e68e5b017d129773aefb5fe285
Mukhathutsheli <charlesndou454@gmail.com>
2025-07-28 23:45:55 +0200

    nn

--- generate-env.js
+++ generate-env.js
@@ -1,5 +1,22 @@
 const fs = require("fs");
 
+const requiredEnvVars = [
+  "FIREBASE_API_KEY",
+  "FIREBASE_AUTH_DOMAIN",
+  "FIREBASE_PROJECT_ID",
+  "FIREBASE_STORAGE_BUCKET",
+  "FIREBASE_MESSAGING_SENDER_ID",
+  "FIREBASE_APP_ID",
+  "FIREBASE_MEASUREMENT_ID",
+];
+
+const missing = requiredEnvVars.filter((key) => !process.env[key]);
+
+if (missing.length > 0) {
+  console.error("Missing required environment variables:", missing.join(", "));
+  process.exit(1); // Fail the build
+}
+
 const env = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -12,4 +29,4 @@ const env = {
 
 fs.writeFileSync("env.js", `window._env_ = ${JSON.stringify(env, null, 2)};`);
 
-
+console.log("env.js generated successfully");
